40add2f004aef061ec4edc18f2699070529f004f
Zuber Masalikar <zuber.masalikar@hotmail.com>
2025-09-18 07:09:56 +0530

    Add GET /api/auth/register friendly message

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { register, login, getUser } = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
-
+router.get("/register", (req, res) => {
+  res.send("This endpoint accepts POST requests. Use Postman or send a JSON POST to register.");
+});
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", authMiddleware, getUser);
