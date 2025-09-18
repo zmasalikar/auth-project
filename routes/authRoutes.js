4a569b9c32150314224f086ded0a19133162216e
Zuber Masalikar <zuber.masalikar@hotmail.com>
2025-09-18 07:21:33 +0530

    Add input validation for register & login

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,35 @@
 const express = require("express");
+const { body } = require("express-validator");
 const router = express.Router();
 const { register, login, getUser } = require("../controllers/authController");
 const authMiddleware = require("../middlewares/authMiddleware");
+const validate = require("../middlewares/validate");
+
 router.get("/register", (req, res) => {
   res.send("This endpoint accepts POST requests. Use Postman or send a JSON POST to register.");
 });
-router.post("/register", register);
-router.post("/login", login);
+
+router.post(
+  "/register",
+  [
+    body("username").notEmpty().withMessage("username is required"),
+    body("email").isEmail().withMessage("valid email is required"),
+    body("password").isLength({ min: 6 }).withMessage("password must be at least 6 chars")
+  ],
+  validate,
+  register
+);
+
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("valid email is required"),
+    body("password").notEmpty().withMessage("password is required")
+  ],
+  validate,
+  login
+);
+
 router.get("/me", authMiddleware, getUser);
 
 module.exports = router;
