00c40127115a81136a4f4fc327723f4d66547e3d
Zuber Masalikar <zuber.masalikar@hotmail.com>
2025-09-18 07:05:18 +0530

    Add redirect from / to /api/auth/register

--- server.js
+++ server.js
@@ -16,3 +16,4 @@ app.use("/api/auth", authRoutes);
 app.listen(process.env.PORT, () =>
   console.log(`Server running on port ${process.env.PORT}`)
 );
+app.get("/", (_req, res) => res.redirect("/api/auth/register"));
